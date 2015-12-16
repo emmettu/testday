84594ede0b4107f185d370057d96cf570d8989fc
Emmett Underhill <emmettu@gmail.com>
2015-12-16 00:57:56 -0500

    Initial Commit
    Have basic text rendering going, nothing else.

---
+++ html/src/com/emmett/testday/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.emmett.testday.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.emmett.testday.Main;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Main();
+        }
+}
